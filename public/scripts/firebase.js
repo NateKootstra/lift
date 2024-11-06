faefa5b56055764630c22c1873452efe35105c88
Nate Kootstra <natekootstra@gmail.com>
2024-11-06 12:16:17 -0500

    Add sign in code

--- public/scripts/firebase.js
+++ public/scripts/firebase.js
@@ -1,9 +1,9 @@
 // Import Firebase libraries.
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
 import { getFirestore, doc, setDoc } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
 
 
-// Set the Firerbase configuration.
 const firebaseConfig = {
     apiKey: "AIzaSyCcyjSl_1cD6LxYVpNwPoyEBUClEGBdzoI",
     authDomain: "liftmanagersite.firebaseapp.com",
@@ -19,6 +19,22 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 // Initialize Firestore.
 const db = getFirestore(app);
+
+
+
+email = "TODO"
+password = "Add username and password input system."
+
+const auth = getAuth();
+signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+        const user = userCredential.user;
+        alert("I think it worked?");
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        alert(errorCode);
+    });
 
 
 
